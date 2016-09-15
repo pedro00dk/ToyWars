7312e79a0a4fb7494fd8d5e4afa824032e95b5ec
Pedro Henrique <pedro00dk@live.com>
2016-09-15 20:54:46 -0300

    Add Puu's models and creates the basic movimentation
    The Puu's gun model too was added, a scene to tests was created.

---
+++ Assets/Scripts/Puu.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody))]
+public class Puu : MonoBehaviour {
+
+	[Header("Animation/Transform Properties")]
+	public Transform columnJoint;
+	public Transform cameraColumnJoint;
+
+	[Header("Movement Properties")]
+	public float speed;
+	public Vector2 sensibility;
+
+	// Joint independent (the up is negated in script)
+	public Vector2 yRotationUpDown;
+
+	// Animatior parameters
+	string walkingAnimParam = "walking";
+	// boolean
+
+	// Components
+	Animator animator;
+	Rigidbody body;
+
+	// Internal properties
+	bool walking;
+
+	// Smooths, lerps and temps
+	Vector3 currentMovementVelocity;
+
+	Vector3 columnIndependentJointLocalEulerAngles;
+
+
+	void Start() {
+		animator = GetComponent<Animator>();
+		body = GetComponent<Rigidbody>();
+		walking = false;
+		SetAnimationParams();
+	}
+
+	void Update() {
+
+		// Movement
+		walking = Input.GetKey(KeyCode.W);
+
+		// X rotation
+		Vector3 localEulerAngles = new Vector3(
+			                           transform.localEulerAngles.x,
+			                           transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensibility.x * Time.deltaTime,
+			                           transform.localEulerAngles.z
+		                           );
+		transform.localEulerAngles = localEulerAngles;
+
+		// Animation
+		SetAnimationParams();
+	}
+
+	void LateUpdate() {
+		// Y rotation (runs after animation)
+		float rotation = Input.GetAxis("Mouse Y") * sensibility.y * Time.deltaTime;
+		columnIndependentJointLocalEulerAngles = new Vector3(
+			Mathf.Clamp(
+				columnIndependentJointLocalEulerAngles.x + rotation,
+				-yRotationUpDown.x, yRotationUpDown.y
+			),
+			0,
+			0
+		);
+		columnJoint.localEulerAngles += columnIndependentJointLocalEulerAngles;
+		cameraColumnJoint.localEulerAngles = columnIndependentJointLocalEulerAngles;
+	}
+
+	void FixedUpdate() {
+
+		// Movement
+		if (walking) {
+			body.MovePosition(
+				Vector3.SmoothDamp(
+					body.position,
+					body.position + transform.forward,
+					ref currentMovementVelocity,
+					0.2f
+				)
+			);
+		}
+	}
+
+	void SetAnimationParams() {
+		animator.SetBool(walkingAnimParam, walking);
+	}
+}
---
+++ Assets/Scripts/PuusGun.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class PuusGun : MonoBehaviour {
+
+	// Components
+	Animator animator;
+
+	// Internal components
+	bool triggered;
+	float triggeredTime;
+
+	void Start() {
+		animator = GetComponent<Animator>();
+		animator.Play("Idle");
+	}
+
+
+
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKey(KeyCode.Space)) {
+			animator.SetTrigger("triggered");
+			animator.ResetTrigger("released");
+		} else {
+			animator.SetTrigger("released");
+			animator.ResetTrigger("triggered");
+		}
+	}
+}
