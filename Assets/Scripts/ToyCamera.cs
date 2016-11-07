e90bfce8b7137de9a929ce823355395c9ed89411
Eduardo Rodrigues <Eduardo Rodrigues>
2016-11-07 12:46:36 -0200

    Now the model gets the main camera of the scene

---
+++ Assets/Scripts/ToyCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToyCamera : MonoBehaviour {
+    private Camera _camera;
+
+	// Use this for initialization
+	void Start () {
+        _camera = FindObjectOfType<Camera>();
+        _camera.transform.parent = this.transform;
+        _camera.transform.position = this.transform.position;//new Vector3(0,0,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
