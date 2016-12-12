32ded86620168c4d7d86bd560173015484bb15d5
Eduardo Rodrigues <Eduardo Rodrigues>
2016-12-12 12:48:56 -0200

    Player set the nickName

---
+++ Assets/Scripts/Lobby/PlayerInfo.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Prototype.NetworkLobby;
+
+public class PlayerInfo : MonoBehaviour {
+    string NickName;
+
+    public Text NickNameInputField;
+
+    LobbyPlayer lp;
+
+	// Use this for initialization
+	void Start () {
+        NickName = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(lp == null)
+            lp = FindObjectOfType<LobbyPlayer>();
+
+        verifyNickname();
+
+    }
+
+    public void verifyNickname()
+    {
+        if (NickNameInputField)
+            NickName = NickNameInputField.text;
+        
+        //if is a valid nickname, the windows desappear
+        if(lp != null && NickName != "" && !NickName.Contains(" "))
+        {
+            lp.OnMyName(NickName);
+        }
+    }
+}
